bc730c77c11e32995fef6b2571f243c4d1210401
xinglong <lixinglong4212@gmail.com>
2021-06-28 11:22:39 +0800

    commit print practices

--- class_practice.py
+++
@@ -1,9 +0,0 @@
-#list
-class_content = ["小张","小王","小黄"]
-print(class_content)
-#dictionary
-class_content = {"小张":90,"小王":78,"小黄":65}#dictionary must include colon symbol
-print(class_content)
-
-
-
